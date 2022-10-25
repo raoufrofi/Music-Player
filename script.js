64c45157d39c58698c5ab3aa8878a999516afe6f
Abdul-Raouf-33 <103753366+Abdul-Raouf-33@users.noreply.github.com>
2022-10-25 00:24:20 -0700

    Change

--- script.js
+++ script.js
@@ -3,40 +3,37 @@ const musicBox = document.querySelector('.music-box')
 const forward = document.querySelector('.fa-forward')
 const backward = document.querySelector('.fa-backward')
 const record = document.getElementById('record')
-const progressContainer = document.querySelector(".cutter")
-const progressBar = document.querySelector(".cutter-inner")
-const volumeContainer = document.querySelector(".volume")
-const volumeBar = document.querySelector(".volume-inner")
+const volumeBtn = document.getElementById("volumeBtn")
+const lengthBtn = document.getElementById('length')
 const audio = document.getElementById('audio')
-
-// MUSIC DURATION
-musicDuration();
-function musicDuration() {
-  console.log(audio.duration);
-}
-
-
+const blurredBg = document.getElementById('blurred-bg')
 
 // Music Array
-let musicNames = ['Poolside', 'Soloside', 'Till', 'What', 'Slipping', '368', 'Call']
+let musicNames = ['mortal', 'kosandra', 'Saw Music', 'warsong', 'legends never die', 'he kahan ka irada']
 
-let index = 3;
+let index = 1;
 
 
 // Load Song First
+loadSong(index)
 let isPlaying = false;
 play.addEventListener('click', onMusic)
 backward.addEventListener('click', backwardMusic)
 forward.addEventListener('click', forwardMusic)
-loadSong(index)
 
 function loadSong(i) {
   audio.src = `Music/${musicNames[i]}.mp3`
-  record.src = `Thumbnail/${musicNames[i]}.png`
-  indexChecker();
+  record.src = `Thumbnail/${musicNames[i]}.jpg`
+  blurredBg.src = `Thumbnail/${musicNames[i]}.jpg`
+  addingRemovingEvent();
   setTitle(i)
+  audio.onloadedmetadata = () => {
+    lengthSetReset()
+  }
 }
 
+
+// TITLE SETTER
 function setTitle(i) {
   document.getElementById('title').innerHTML = musicNames[i];
 }
@@ -48,35 +45,6 @@ function onMusic() {
   else if (isPlaying == true) {
     pauseMusic()
   }
-}
-
-
-// FORWARD | BACKWARD MUSIC
-function forwardMusic() {
-  index++
-  backward.classList.remove('disabled')
-  backward.addEventListener('click', backwardMusic)
-  attributeSetter(index)
-  playMusic()
-  indexChecker()
-  setTitle(index)
-}
-
-function backwardMusic() {
-  index--
-  console.log(index);
-  forward.addEventListener('click', forwardMusic)
-  forward.classList.remove('disabled')
-  attributeSetter(index)
-  playMusic()
-  indexChecker()
-  setTitle(index)
-}
-
-// ATTRIBUTE SETTER
-function attributeSetter(i) {
-  audio.src = `Music/${musicNames[i]}.mp3`
-  record.src = `Thumbnail/${musicNames[i]}.png`
 }
 
 // PLAY | PAUSE MUSIC
@@ -95,7 +63,46 @@ function pauseMusic() {
 }
 
 
-function indexChecker() {
+// FORWARD | BACKWARD MUSIC
+function forwardMusic() {
+  index++
+  // console.log(index);
+  backward.addEventListener('click', backwardMusic)
+  backward.classList.remove('disabled')
+  attributeSetter(index)
+  playMusic()
+  addingRemovingEvent()
+  setTitle(index)
+  audio.onloadedmetadata = () => {
+    lengthSetReset()
+  }
+}
+
+function backwardMusic() {
+  index--
+  // console.log(index);
+  forward.addEventListener('click', forwardMusic)
+  forward.classList.remove('disabled')
+  attributeSetter(index)
+  playMusic()
+  addingRemovingEvent()
+  setTitle(index)
+  audio.onloadedmetadata = () => {
+    lengthSetReset()
+  }
+}
+
+// ATTRIBUTE SETTER
+function attributeSetter(i) {
+  audio.src = `Music/${musicNames[i]}.mp3`
+  record.src = `Thumbnail/${musicNames[i]}.jpg`
+  blurredBg.src = `Thumbnail/${musicNames[i]}.jpg`
+}
+
+
+
+// INDEX CHECKER
+function addingRemovingEvent() {
   if (index == 0) {
     backward.classList.add('disabled')
     backward.removeEventListener('click', backwardMusic)
@@ -108,39 +115,33 @@ function indexChecker() {
   }
 }
 
-function progressUpdate(e) {
-  const { duration, currentTime } = e.srcElement
-  const width = (currentTime / duration) * 100
-  progressBar.style.width = `${width}%`
-  if (currentTime == duration) {
-    forwardMusic();
+audio.addEventListener('timeupdate', progressUpdate)
+function progressUpdate() {
+  lengthBtn.value = Math.floor(audio.currentTime);
+  if (audio.currentTime == audio.duration) {
+    forwardMusic()
   }
 }
 
-
-function changeProgress(e) {
-  const width = this.clientWidth;
-  const duration = audio.duration;
-  const clickX = e.offsetX;
-  console.log(clickX);
-  audio.currentTime = (clickX / width) * duration;
+lengthBtn.addEventListener('change', changeLength)
+// lengthBtn.addEventListener('mousemove', changeLength)
+function changeLength() {
+  audio.currentTime = lengthBtn.value
+  progressUpdate()
 }
 
-function changeVolume(e) {
-  const width = this.clientWidth;
-  const volume = audio.volume;
-  const clickX = e.offsetX;
-  console.log(clickX);
-  audio.volume = (clickX / width) * 1;
-  volumeBar.style.width = `${audio.volume * 100}%`;
-
+function lengthSetReset() {
+  lengthBtn.value = 0
+  lengthBtn.max = Math.floor(audio.duration)
 }
 
-volumeBar.style.width = `${audio.volume * 100}%`;
-audio.addEventListener('timeupdate', progressUpdate)
-progressContainer.addEventListener('click', changeProgress)
-volumeContainer.addEventListener('click', changeVolume)
-
+// VOLUME CHANGER
+audio.volume = volumeBtn.value / 10;
+volumeBtn.addEventListener('change', changeVolume)
+volumeBtn.addEventListener('mousemove', changeVolume)
+function changeVolume() {
+  audio.volume = this.value / 10
+}
 
 
 
